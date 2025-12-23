7bf4ba60372038ab88be0383a76db3895a36b962
ali_fahmi <k.c.o.c.3000@gmail.com>
2025-12-23 15:04:08 +0530

    masonry fix wibKit bug

--- components/fragments/Masonry.tsx
+++ components/fragments/Masonry.tsx
@@ -12,31 +12,24 @@ const Masonry = () => {
         dir="ltr"
         className="gap-8 px-[30px] columns-2 md:gap-16 md:columns-3 "
       >
-        {prototypes.map((prototype, index) => (
-          <div
-            key={index}
-            className="break-inside-avoid mb-8 md:mb-16"
-            style={{
-              display:
-                breakpoint == "sm" && prototype.height.small === 0
-                  ? "none"
-                  : "block",
-            }}
-          >
-            <img
-              src={prototype.src}
-              alt={prototype.alt}
-              style={{
-                height:
-                  breakpoint === "lg"
-                    ? prototype.height.large
-                    : prototype.height.small,
-                width: "100%",
-              }}
-              className="rounded-md object-cover outline outline-black block"
-            />
-          </div>
-        ))}
+        {prototypes
+          .filter((p) => !(breakpoint === "sm" && p.height.small === 0))
+          .map((prototype, index) => (
+            <div key={index} className="break-inside-avoid mb-8 md:mb-16">
+              <img
+                src={prototype.src}
+                alt={prototype.alt}
+                style={{
+                  height:
+                    breakpoint === "lg"
+                      ? prototype.height.large
+                      : prototype.height.small,
+                  width: "100%",
+                }}
+                className="rounded-md object-cover outline outline-black block"
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
